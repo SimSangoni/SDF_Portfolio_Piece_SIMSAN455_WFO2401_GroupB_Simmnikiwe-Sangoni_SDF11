229aad9a21ae63c743a2b4a95be7db656639f7d2
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-02-21 14:11:34 +0200

    create javascript for event listener for burger menu, update element for event listener

---
+++ 11_portfolio_piece_1/javascript.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu-toggle').addEventListener(click, function(){
+    // shows it when it is in mobile view, but outside of that it doesn't show it
+    document.getElementById('mobile-menu'.classList('hidden'))
+})
