56718a3e3c8702f197edd9d0242b8fd3a8b8ec93
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-02-21 16:04:58 +0200

    component 1 complete

--- 11_portfolio_piece_1/javascript.js
+++ 11_portfolio_piece_1/javascript.js
@@ -1,4 +1,4 @@
-document.getElementById('mobile-menu-toggle').addEventListener(click, function(){
+document.getElementById('mobile-menu-toggle').addEventListener("click", function(){
     // shows it when it is in mobile view, but outside of that it doesn't show it
     document.getElementById('mobile-menu').classList.toggle('hidden');
 })
