a1a2d5ec7ab50346c663ab5e3c469d44b18a6603
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-02-22 20:47:52 +0200

    static stats done--counting included (need to fix the mill and 2.8k later).

--- 11_portfolio_piece_1/javascript.js
+++ 11_portfolio_piece_1/javascript.js
@@ -2,3 +2,15 @@ document.getElementById('mobile-menu-toggle').addEventListener("click", function
     // shows it when it is in mobile view, but outside of that it doesn't show it
     document.getElementById('mobile-menu').classList.toggle('hidden');
 })
+
+var spanElement = document.getElementById('count-stat-1');
+var countToValue = spanElement.getAttribute('data-countto');
+
+var spanElement = document.getElementById('count-stat-2');
+var countToValue = spanElement.getAttribute('data-countto');
+
+var spanElement = document.getElementById('count-stat-3');
+var countToValue = spanElement.getAttribute('data-countto');
+
+var spanElement = document.getElementById('count-stat-4');
+var countToValue = spanElement.getAttribute('data-countto');
